fd8cda8fad1d123d628b454fea3699b4f1b5c84b
Janus S. Valberg-Madsen <janusvm@gmail.com>
2022-01-30 16:31:08 +0100

    💄 Change default font styles (again)

--- settings/appearance.el
+++ settings/appearance.el
@@ -1,7 +1,7 @@
 ;;; settings/appearance.el -*- lexical-binding: t; -*-
 
 ;; FONT SETTINGS ---------------------------------------------------------------
-(setq doom-font (font-spec :family "Iosevka Nova" :size 32)
+(setq doom-font (font-spec :family "Iosevka Nova" :size 18)
       doom-themes-treemacs-enable-variable-pitch nil
       display-line-numbers-type nil)
 
